c73308bbf37c2934d792bc5e33835885ca4b61cb
AT <AT@139.com>
2021-05-15 11:02:24 +0800

    first commit

---
+++ 01_CSS2.1/00_试题/001_3_FindBug()来找bug.js
@@ -0,0 +1,7 @@
+//模拟版本数组
+var verArray = [1, 2, 3, 4, 5, 6, 7];
+for (let i = 1; i < verArray.length + 1; i++) {
+  if (FindBug(verArray[i]))
+    // 输出版本号，可另外封装一个方法来进行后续操作
+    return ver;
+}
---
+++ 02_CSS3.0/05.动画/04.imgData.js
@@ -0,0 +1,18 @@
+﻿var imgSrcs = {
+    aqua: [
+        '../../pic/pic/Aqua.jpg',
+        '../../pic/pic/Aqua_light.png'
+    ],
+    red: [
+        '../../pic/pic/Red.jpg',
+        '../../pic/pic/Red2.jpg'
+    ],
+    other: [
+        '../../pic/pic/Flag.jpg',
+        '../../pic/pic/Kiana.png',
+        '../../pic/pic/xp.jpg',
+        '../../pic/pic/YaeSakura.jpg',
+        '../../pic/pic/Yume.jpg',
+    ]
+
+}
